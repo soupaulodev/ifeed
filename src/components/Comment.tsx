3ee32171f109d5e8ece9def3293c3b7ee82396ff
Paulo Marques <paulomarques.rs1@gmail.com>
2024-06-11 14:31:01 -0300

    feat: add component Comment

---
+++ src/components/Comment.tsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar src="https://github.com/soupaulodev.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Paulo Marques</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Paulo, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
