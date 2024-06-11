4f2db4442848db71e16ceb959a446fa7ea8460f8
Paulo Marques <paulomarques.rs1@gmail.com>
2024-06-11 14:31:18 -0300

    feat: add component Comment

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,5 @@
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -5,11 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/soupaulodev.png"
-            alt="Foto de perfil do Paulo"
-          />
+          <Avatar hasBorder src="https://github.com/soupaulodev.png" />
           <div className={styles.authorInfo}>
             <strong>Paulo Marques</strong>
             <span>Web Developer</span>
@@ -52,6 +50,11 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+      <div className="commentList">
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 import { PencilLine } from "phosphor-react";
 
@@ -9,11 +10,7 @@ export function Sidebar() {
         src="https://media.licdn.com/dms/image/D4D16AQGFrTuoZV_Vkg/profile-displaybackgroundimage-shrink_350_1400/0/1711853783886?e=1723680000&v=beta&t=SZVcx_EtORgNfye7_tUV5T12XQKpEigGdLWTXet5iM4"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/soupaulodev.png"
-          alt="Foto de perfil do Paulo"
-        />
+        <Avatar hasBorder src="https://github.com/soupaulodev.png" />
         <strong>Paulo Marques</strong>
         <span>Web Developer</span>
         <footer>
