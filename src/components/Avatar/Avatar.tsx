3adac5a84ceb3e5326ec402f9a9d82672f167dda
Paulo Marques <paulomarques.rs1@gmail.com>
2024-06-12 23:26:12 -0300

    feat: extendendo interfaces

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,19 +1,21 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.css";
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
   src: string;
 }
 
-export function Avatar({
-  hasBorder,
-  src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-}: AvatarProps) {
+export function Avatar({ hasBorder, src, ...props }: AvatarProps) {
+  const srcImage =
+    src ||
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt=""
+      src={srcImage}
+      {...props}
     />
   );
 }
